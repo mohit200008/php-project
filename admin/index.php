f419470579c9fddfe23f3ae7714bc1820a647f7f
mohit <mohitklamba@gmail.com>
2021-04-07 23:50:38 +0530

    php project started

---
+++ admin/index.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+        <title>Food Order Website- Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+         <div class="menu text-center">
+           <div class="wrapper">
+
+           <ul>
+               <li><a href="#">Home</a></li>
+               <li><a href="#"> Admin</a></li>
+               <li><a href="#">Category</a></li>
+               <li><a href="#">Food</a></li>
+               <li><a href="#">Order</a></li>
+           </ul>
+         
+         </div>
+         </div>
+
+         <div class="main-content">
+         <div class="wrapper">
+         <h1>DASHBOARD</h1>
+
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+
+         </div>
+         <div class="clearfix">
+             
+
+         </div>
+         </div>
+
+         <div class="footer">
+         <div class="wrapper">
+           <p class=text-center>2020 All rights reserved,Food-Court Developed By- <a href="#">ML-ASSASN</a></p>
+         
+         </div>
+         </div>
+    </body>     
+</html>
