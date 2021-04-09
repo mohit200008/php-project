95309f8e685265208fa3ad81706672246b23a12d
mohit <mohitklamba@gmail.com>
2021-04-10 01:31:56 +0530

    add admin complete

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -6,6 +6,16 @@
          <br>
          <br>
 
+         <?php 
+             if(isset($_SESSION['add']))
+             {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+             }
+             ?>
+             <br>
+             <br>
+
          <a href="add-admin.php" class="btn-primary">Add Admin</a>
 
          <br>
@@ -14,7 +24,7 @@
          <table class="tbl-full">
              <tr>
                 <th>S.NO</th>
-                <th>Full Name</th>
+                <th>Full_Name</th>
                 <th>UserName</th>
                 <th>Actions</th>
              
